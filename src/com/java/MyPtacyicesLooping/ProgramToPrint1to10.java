af323f707844cbfd79a81077e47ffa2267800d99
Bakyt Aybekovich <myworkspace8888@gmail.com>
2023-03-02 10:02:54 +0600

    added looping practice

---
+++ src/com/java/MyPtacyicesLooping/ProgramToPrint1to10.java
@@ -0,0 +1,12 @@
+package com.java.MyPtacyicesLooping;
+
+// Write a program to print 1 to 10
+
+public class ProgramToPrint1to10 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i != 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
